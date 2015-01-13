154ad63fa468ce486475c08e6dab702130f93106
Mark Stapper <mark.stapper@imtechmarine.com>
2015-01-13 16:40:24 +0100

    Improve regex and try regex iterator
    Change for loop to build the regex that matches the version portion of the
    versionString
    Add simple use of regex_iterator to count matches of single sub version
    regexes

--- lib/Version.cpp
+++ lib/Version.cpp
@@ -15,13 +15,19 @@ Version::Version(const string &versionString)
 	// Find one or more digits with 0 or more trailing characters
 	// Concatenating this string finds anything in the form of 1 1.2.3 or even 1,2.3.27-2a0123[12
 	string digitsString = "([[:digit:]]+)";
-	string seperatorString = "(.{1})";
-	string regexString = digitsString;
-	regex versionRegex(regexString);
+	string seperatorString = "([^[:digit:]]{1}|$)";
+	string regexBuilderString = digitsString+seperatorString;
+	string regexString;
 	int nrOfDigits;
-	//find the number of digits
-	for (nrOfDigits = 0; regex_search(versionString, regex(regexString)); ++nrOfDigits)
-		regexString += seperatorString+digitsString;
 	
+	//find the number of digits
+	for (nrOfDigits = 0; regex_search(versionString, regex(regexString + regexBuilderString)); ++nrOfDigits)
+		regexString += regexBuilderString;
+	
+	auto digits_regex = regex(digitsString + seperatorString);
+	auto digit_finder = sregex_iterator(versionString.begin(), versionString.end(), digits_regex);
+	auto digit_end = sregex_iterator();
+	int matches = distance(digit_finder, digit_end);
+
 	throw(BaseExceptions::NotImplementedException(__FILE__, __LINE__));
 }
