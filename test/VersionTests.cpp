61c6ec1f84002253fb5ac8f176e3d42f23b3a5f5
Mark Stapper <mark.stapper@imtechmarine.com>
2015-01-14 12:23:07 +0100

    Add numerical constructed Version objects for comparison

--- test/VersionTests.cpp
+++ test/VersionTests.cpp
@@ -3,10 +3,21 @@
 
 VersionTests::VersionTests()
 {
-	Version numeric{ 1, 2, 3, 4 };
-	Version bareString{ "1.2.3.4" };
+	Version SingleNumeric{ 1 };
+	Version bareSingleString{ "1" };
+	
+	Version numeric{ 21, 22, 23, 24 };
+	Version bareString{ "21.22.23.24" };
+
+	Version seperated{ 1, 2, 3, 4, 123, 15 };
 	Version seperatedString{ "1/2-3.4_123+15" };
+
+	Version prefixed{ 15, 9, 3, 18, 3 };
 	Version prefixedString{ "release/15.9/3.18-3" };
+
+	Version postfixed{ 1, 2, 3 };
 	Version postfixedString{ "1.2.3-killmenow" };
+
+	Version gitDescribe{ 1, 2, 3 , 123 };
 	Version gitDescribeString{ "1.2.3-123-ac1337ff" };
 }
