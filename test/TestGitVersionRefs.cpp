c7a7f4dcc1354cbc02b21b27b990381eade9f0b4
Mark Stapper <stark@mapper.nl>
2015-02-11 16:01:33 +0100

    Fix getLatest tests

--- test/TestGitVersionRefs.cpp
+++ test/TestGitVersionRefs.cpp
@@ -2,9 +2,11 @@
 #include "GitVersionRefs.h"
 #include "FileLineException.h"
 #include <algorithm>
-#define THROW_IF_EQUAL(A,B) if (A==B) throw(BaseExceptions::FileLineException("GitVersionTest failed",__FILE__,__LINE__))
-#define THROW_IF_UNEQUAL(A,B) if (A!=B) throw(BaseExceptions::FileLineException("GitVersionTest failed",__FILE__,__LINE__))
-#define THROW_IF_NOTEQUAL(A,B) if (!(A==B)) throw(BaseExceptions::FileLineException("GitVersionTest failed",__FILE__,__LINE__))
+#include <iostream>
+#include "to_string.hpp"
+#define THROW_IF_EQUAL(A,B) { std::cout << "Testing \t" << A << " != " << B<<endl; if (A==B) throw(BaseExceptions::FileLineException("VersionTest failed\n"+to_string(A)+"\t==\t"+to_string(B),__FILE__,__LINE__)); }
+#define THROW_IF_UNEQUAL(A,B) { std::cout << "Testing \t" << A << " == " << B <<endl; if (A!=B) throw(BaseExceptions::FileLineException("VersionTest failed\n"+to_string(A)+"\t!=\t"+to_string(B),__FILE__,__LINE__)); }
+#define THROW_IF_NOTEQUAL(A,B) { std::cout << "Testing \t" << A << " == " << B<<endl; if (!(A==B)) throw(BaseExceptions::FileLineException("VersionTest failed\n"+to_string(A)+"\tNOT ==\t"+to_string(B),__FILE__,__LINE__)); }
 
 /// Test class to allow data injection
 class GVRTest : public GitVersionRefs
@@ -26,30 +28,9 @@ TestGitVersionRefs::TestGitVersionRefs()
 void TestGitVersionRefs::initTestData()
 {
 	testRefs.push_back({25,17});
-	Version testVersion = {0, 0};
-	for (int count = 1; count <= 5; ++count)
-		testRefs.push_back(++testVersion);
-	testVersion.demote();
-	++testVersion;
-	testVersion.promote();
-	for (int count = 1; count <= 5; ++count)
-		testRefs.push_back(++testVersion);
-		testVersion.demote();
-	++testVersion;
-	testVersion.promote();
-	for (int count = 1; count <= 5; ++count)
-		testRefs.push_back(++testVersion);
-		testVersion.demote();
-	++testVersion;
-	testVersion.promote();
-	for (int count = 1; count <= 5; ++count)
-		testRefs.push_back(++testVersion);
-		testVersion.demote();
-	++testVersion;
-	testVersion.promote();
-	for (int count = 1; count <= 5; ++count)
-		testRefs.push_back(++testVersion);
-
+	for (int major = 1;major <= 5; major++)
+		for (int count = 0; count <= 5; ++count)
+			testRefs.push_back({major,count});
 }
 void TestGitVersionRefs::testTags()
 {
@@ -58,17 +39,24 @@ void TestGitVersionRefs::testTags()
 		localRefs.push_back({ 0, count });
 	GitVersionRefs refs;
 	THROW_IF_UNEQUAL(refs.getRefs(), localRefs);
-
 }
 
 void TestGitVersionRefs::testGetLatest()
 {
 	GVRTest refTest = testRefs;
 	THROW_IF_UNEQUAL(refTest.getLatest(), Version({25,17}));
-	for (int i = 0; i < 5; i++)
+	// If the version number is lower than the lowest version available, the lowest existing release is returned
+	THROW_IF_UNEQUAL(refTest.getLatest({0}), Version({1,0}));
+	
+	for (int i = 1; i < 6; i++)
 		THROW_IF_UNEQUAL(refTest.getLatest({i}), Version({i,5}));
-
-	// What to do when the requested release doesn't exist yet?
+	
+	// There is no version 6, so it should return the last version not higher, or should it?
+	THROW_IF_UNEQUAL(refTest.getLatest({6}), Version({5,5}));
+	
+	// Should return the highest version number when a crazy high version number is queried for
+	THROW_IF_UNEQUAL(refTest.getLatest({5000}), refTest.getLatest());
+	
 }
 
 TestGitVersionRefs::~TestGitVersionRefs()
