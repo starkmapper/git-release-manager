508264f6c5b184f18e30d37f31c8aaddde60a3c8
Mark Stapper <stark@mapper.nl>
2015-02-12 09:25:37 +0100

    Remove verbosity from tests, should make it optional later

--- test/TestGitVersionRefs.cpp
+++ test/TestGitVersionRefs.cpp
@@ -4,9 +4,9 @@
 #include <algorithm>
 #include <iostream>
 #include "to_string.hpp"
-#define THROW_IF_EQUAL(A,B) { std::cout << "Testing \t" << A << " != " << B<<endl; if (A==B) throw(BaseExceptions::FileLineException("VersionTest failed\n"+to_string(A)+"\t==\t"+to_string(B),__FILE__,__LINE__)); }
-#define THROW_IF_UNEQUAL(A,B) { std::cout << "Testing \t" << A << " == " << B <<endl; if (A!=B) throw(BaseExceptions::FileLineException("VersionTest failed\n"+to_string(A)+"\t!=\t"+to_string(B),__FILE__,__LINE__)); }
-#define THROW_IF_NOTEQUAL(A,B) { std::cout << "Testing \t" << A << " == " << B<<endl; if (!(A==B)) throw(BaseExceptions::FileLineException("VersionTest failed\n"+to_string(A)+"\tNOT ==\t"+to_string(B),__FILE__,__LINE__)); }
+#define THROW_IF_EQUAL(A,B) if (A==B) throw(BaseExceptions::FileLineException("VersionTest failed\n"+to_string(A)+"\t==\t"+to_string(B),__FILE__,__LINE__))
+#define THROW_IF_UNEQUAL(A,B) if (A!=B) throw(BaseExceptions::FileLineException("VersionTest failed\n"+to_string(A)+"\t!=\t"+to_string(B),__FILE__,__LINE__))
+#define THROW_IF_NOTEQUAL(A,B) if (!(A==B)) throw(BaseExceptions::FileLineException("VersionTest failed\n"+to_string(A)+"\tNOT ==\t"+to_string(B),__FILE__,__LINE__))
 
 /// Test class to allow data injection
 class GVRTest : public GitVersionRefs
